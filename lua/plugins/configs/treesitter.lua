2bf4e6e275eb9f94110c78a863f955c7148cf93e
Thorben Bender <bender.thorben0@gmail.com>
2023-08-22 12:32:13 +0200

    init config

---
+++ lua/plugins/configs/treesitter.lua
@@ -0,0 +1,29 @@
+local options = {
+  ensure_installed = {
+    "lua",
+    "vim",
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "tsx",
+    "json",
+    "c",
+    "zig",
+    "rust",
+    "gitignore",
+    "hlsl",
+    "svelte",
+    "dockerfile",
+    "glsl",
+  },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options
