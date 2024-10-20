08938637591467639bed01e0494786f5cbebeb25
Thorben Bender <thorben@codigo.de>
2024-10-20 14:26:42 +0200

    Fix telescope config that causes the error

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -33,7 +33,7 @@ local options = {
     file_sorter = require("telescope.sorters").get_fuzzy_file,
     file_ignore_patterns = { "node_modules" },
     generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
-    path_display = { "truncate" },
+    path_display = "truncate",
     winblend = 0,
     border = {},
     borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
@@ -58,7 +58,7 @@ local options = {
     },
   },
 
-  extensions_list = { "themes", "terms" },
+  extensions_list = { "themes", "terms", "git_worktree" },
 }
 
 return options
