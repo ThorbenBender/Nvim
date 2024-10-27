94083ce4509ba04d7b49b6245dbe7f3ba18cf783
Thorben Bender <bender.thorben0@gmail.com>
2024-10-27 22:15:35 +0100

    remove nvimtree mapping

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -242,18 +242,6 @@ M.lspconfig = {
   },
 }
 
-M.nvimtree = {
-  plugin = true,
-
-  n = {
-    -- toggle
-    ["<C-n>"] = { "<cmd> NvimTreeToggle <CR>", "Toggle nvimtree" },
-
-    -- focus
-    ["<leader>e"] = { "<cmd> NvimTreeFocus <CR>", "Focus nvimtree" },
-  },
-}
-
 M.telescope = {
   plugin = true,
 
